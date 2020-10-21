731327f62fbf0d5de7188da5732a8aa3c477d1b2
Somphoph <Somphoph@users.noreply.github.com>
2020-10-21 11:24:44 +0700

    Test - Add test Unmarshal ReplyMsg

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,5 +19,27 @@ func TestHttpPostMsg(t *testing.T) {
 		if status := resp.Code; status != http.StatusOK {
 			t.Errorf("Wrong code: got %v want %v", status, http.StatusOK)
 		}
+	})
+}
+
+func TestReplyMsg(t *testing.T) {
+	t.Run("it should found ReplyMsg", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodPost, "/msg", nil)
+		if err != nil {
+			t.Error(err)
+		}
+		resp := httptest.NewRecorder()
+		handler := http.HandlerFunc(msgHandler)
+		handler.ServeHTTP(resp, req)
+		if status := resp.Code; status != http.StatusOK {
+			t.Errorf("Wrong code: got %v want %v", status, http.StatusOK)
+		}
+
+		var replyMsg ReplyMsg
+		err = json.Unmarshal(resp.Body.Bytes(), &replyMsg)
+		if err != nil {
+			t.Errorf("Can't unmarshal to type ReplyMsg found error %v", err)
+		}
+
 	})
 }
